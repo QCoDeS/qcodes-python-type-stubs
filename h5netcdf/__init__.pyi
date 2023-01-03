b32c4fc91ede4163f3c823cf529322e1f173f07b
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2023-01-03 13:10:16 +0100

    h5netcdf stubs reexport symbols

--- h5netcdf/__init__.pyi
+++ h5netcdf/__init__.pyi
@@ -11,3 +11,5 @@ h5netcdf
 A Python library for the netCDF4 file-format that directly reads and writes
 HDF5 files via h5py, without using the Unidata netCDF library.
 """
+
+__all__ = ["CompatibilityError", "Dimension", "File", "Group", "Variable"]
