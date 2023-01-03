3bf24868ebb1c730292abeb6a61652687b9cc197
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2023-01-03 13:08:47 +0100

    wrapt stubs reexport symbols

--- wrapt/__init__.pyi
+++ wrapt/__init__.pyi
@@ -10,3 +10,27 @@ from .arguments import formatargspec
 
 __version_info__ = ...
 __version__ = ...
+
+__all__ = ["formatargspec",
+"AdapterFactory",
+"adapter_factory",
+"decorator",
+"synchronized",
+    "discover_post_import_hooks",
+    "notify_module_loaded",
+    "register_post_import_hook",
+    "when_imported",
+    "BoundFunctionWrapper",
+    "CallableObjectProxy",
+    "FunctionWrapper",
+    "ObjectProxy",
+    "PartialCallableObjectProxy",
+    "WeakFunctionProxy",
+    "apply_patch",
+    "function_wrapper",
+    "patch_function_wrapper",
+    "resolve_path",
+    "transient_function_wrapper",
+    "wrap_function_wrapper",
+    "wrap_object",
+    "wrap_object_attribute",]
